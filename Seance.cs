5bef5a2b66278b7dfbabd5b19412909e49435352
rreiter <rreiter@S-204-16>
2019-12-16 11:57:07 +0100

    class seance completer

--- Seance.cs
+++ Seance.cs
@@ -8,9 +8,14 @@ namespace plannning_ligue
 {
     class Seance
     {
-        public Seance(string ChoixJour , string JourDebut , string JourFin , string HeureD , string MinuteD , string HeureF, string MinuteF, string Type ,string desc)
+        public Seance(string ChoixJour , DateTime JourDebut , DateTime JourFin , string HeureD , string MinuteD , string HeureF, string MinuteF, string Type ,string desc)
         {
-
+            // foreach ou for a faire ....
+            Sql Unnom = new Sql();
+        }
+        public string switchFormatDate(DateTime Date)
+        { 
+            return Date.ToString("YYYY-MM-dd");
         }
     }
 }
